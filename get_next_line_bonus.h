e9bda6e40aec6b359df9c23444ace8c23cc9a8f1
Александра Савастьянова <avsavastyanova@edu.hse.ru>
2021-11-21 23:06:25 +0300

    1234

---
+++ get_next_line_bonus.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+#include <stdio.h>////////
+
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 12
+// #endif
+
+#endif
